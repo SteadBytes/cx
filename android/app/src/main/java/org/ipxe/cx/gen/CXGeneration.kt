1a9ccc3556bcf671e297ff4b37b4f73e267c95ff
Ben Steadman <steadmanben1@gmail.com>
2020-04-12 19:59:28 +0100

    android: Implement Contact Identifier Generator
    Introduces an implementations of both Generator Types as specified by
    section 3 of the CX specification.
    
    Signed-off-by: Ben Steadman <steadmanben1@gmail.com>

--- android/app/src/main/java/org/ipxe/cx/gen/CXGeneration.kt
+++ android/app/src/main/java/org/ipxe/cx/gen/CXGeneration.kt
@@ -1,7 +1,44 @@
 package org.ipxe.cx.gen
 
+import org.spongycastle.crypto.engines.AESEngine
 import org.spongycastle.crypto.prng.EntropySource
 import org.spongycastle.crypto.prng.EntropySourceProvider
+import org.spongycastle.crypto.prng.drbg.CTRSP800DRBG
+import java.nio.ByteBuffer
+import java.util.*
+import kotlin.experimental.and
+import kotlin.experimental.inv
+import kotlin.experimental.or
+
+
+private const val UUID_BYTES_LEN = 16
+
+
+/**
+ * Instantiate a Type 1 Generator as defined by sections 3.1 and 3.2 of the CX specification.
+ */
+fun cxGen1(seed: ByteArray): CXGenerator<Type1> {
+    return CXGenerator(
+        FixedDRBG(
+            Type1, Seed.fromBytes(
+                Type1, seed
+            )
+        )
+    )
+}
+
+/**
+ * Instantiate a Type 2 Generator as defined by sections 3.1 and 3.1 of the CX specification.
+ */
+fun cxGen2(seed: ByteArray): CXGenerator<Type2> {
+    return CXGenerator(
+        FixedDRBG(
+            Type2, Seed.fromBytes(
+                Type2, seed
+            )
+        )
+    )
+}
 
 
 /**
@@ -29,6 +66,128 @@ object Type1 : GeneratorType(
 object Type2 : GeneratorType(
     256, 256, 32, 16, 2048
 )
+
+
+/**
+ * Contact Identifier Generator as described by section 3 of the CX specification.
+ *
+ * This should be instantiated via one of the public helper functions e.g. [cxGen1] to ensure the
+ * correct ownership and usage of the underlying generator and seed values (hence the `internal`
+ * constructor).
+ */
+class CXGenerator<T : GeneratorType> internal constructor(private val rng: FixedDRBG<T>) {
+    fun iterate(): UUID {
+        // Generate next bytes to base Contact Identifier on
+        val bytes = ByteArray(UUID_BYTES_LEN)
+        rng.generate(bytes)
+        // Set fixed bits for version 4 UUID
+        bytes[8] = (bytes[8] and 0xc0.toByte().inv()).or(0x80.toByte()) // clock_seq_hi_and_reserved
+        bytes[6] = (bytes[6] and 0xf0.toByte().inv()).or(0x40.toByte()) // time_hi_and_version
+        // Instantiate a Java UUID - no direct constructor from a single ByteArray is provided,
+        // instead the API for manual UUID instantiation requires specifying two Long values
+        val buf = ByteBuffer.wrap(bytes) // Wrap in a ByteBuffer to avoid manual parsing of Longs
+        val high = buf.long // e.g. side-effectful buf.getLong() advances internal state of buf
+        val low = buf.long
+        return UUID(high, low)
+    }
+}
+
+
+/**
+ * Represents a seed value constructed from some fixed entropy and a nonce value as described in
+ * section 3 of the CX specification. The primary role of this class is to ensure the invariants
+ * specified for seed length, entropy length and nonce length are upheld.
+ */
+class Seed<T : GeneratorType> internal constructor(
+    type: T,
+    val entropyInput: ByteArray,
+    val nonce: ByteArray
+) {
+    init {
+        if (entropyInput.size != type.entropyInputLen) {
+            throw IllegalArgumentException(
+                "entropyInput length must match the value specified by the Generator Type"
+            )
+        }
+        if (nonce.size != type.nonceLen) {
+            throw IllegalArgumentException(
+                "nonce length must match the value specified by the Generator Type"
+            )
+        }
+    }
+
+    companion object {
+        /**
+         * Instantiate a [Seed] from a complete seed value from which [entropyInput] and [nonce]
+         * will be parsed according to the given [GeneratorType].
+         */
+        fun <T : GeneratorType> fromBytes(type: T, seed: ByteArray): Seed<T> {
+            if (seed.size != type.seedLen) {
+                throw IllegalArgumentException(
+                    "seed length must match the value specified by the Generator Type"
+                )
+            }
+            return Seed(
+                type,
+                seed.copyOfRange(0, type.entropyInputLen),
+                seed.copyOfRange(type.entropyInputLen, type.seedLen)
+            )
+        }
+    }
+}
+
+
+class GeneratorExhaustedException(message: String) : Exception(message)
+
+
+/**
+ * DRBG with a fixed entropy source and maximum iteration limit as specified by section 3 of the
+ * CX specification.
+ */
+class FixedDRBG<T : GeneratorType> internal constructor(
+    private val type: T,
+    private val seed: Seed<T>
+) {
+    private val entropySource =
+        FixedEntropySourceProvider(seed.entropyInput).get(seed.entropyInput.size * 8)
+    private var currentIterations = 0
+
+    private val rng = when (type as GeneratorType) {
+        Type1 -> {
+            CTRSP800DRBG(
+                AESEngine(),
+                type.securityStrength,
+                type.securityStrength,
+                entropySource,
+                null,
+                seed.nonce
+            )
+        }
+        Type2 -> {
+            CTRSP800DRBG(
+                AESEngine(),
+                type.securityStrength,
+                type.securityStrength,
+                entropySource,
+                null,
+                seed.nonce
+            )
+        }
+    }
+
+    /**
+     * Populate a passed in array with randomly generated data.
+     *
+     * @throws [GeneratorExhaustedException] If the iteration limit for [type] has been reached.
+     */
+    fun generate(output: ByteArray) {
+        if (currentIterations >= type.maxIterations) {
+            throw GeneratorExhaustedException("${type.maxIterations} iteration limit reached")
+        }
+        currentIterations += 1
+        rng.generate(output, null, false)
+    }
+}
 
 
 class EntropySourceExhaustedException(message: String) : Exception(message)
